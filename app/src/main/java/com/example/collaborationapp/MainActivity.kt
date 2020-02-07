8d09cdf21ce16633bb85624f5133f75559a8469b
yasirbensanad <kingsanad1804>
2020-02-07 10:29:04 +0700

    Tambah Intent Jika Click Gambar Ucup

--- app/src/main/java/com/example/collaborationapp/MainActivity.kt
+++ app/src/main/java/com/example/collaborationapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.collaborationapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +13,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(this,"ciontoh oas",Toast.LENGTH_LONG).show()
+
+        imgucup.setOnClickListener {
+            startActivity(Intent(this, Main2Activity::class.java))
+        }
     }
+
 
 }
