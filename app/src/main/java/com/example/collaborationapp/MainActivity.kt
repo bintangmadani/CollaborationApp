ee22eb40b3e19bfd84b745a5f3727fb41bfb249d
yasirbensanad <kingsanad1804>
2020-02-07 10:17:49 +0700

    Tambah Toast di MainActivity

--- app/src/main/java/com/example/collaborationapp/MainActivity.kt
+++ app/src/main/java/com/example/collaborationapp/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.collaborationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"ciontoh oas",Toast.LENGTH_LONG).show()
     }
+
 }
